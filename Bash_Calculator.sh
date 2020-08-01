0963f19e185004d3d1512f4b816c42760a185634
Noel Zacharia <noelzacharia8@gmail.com>
2020-08-01 19:34:20 -0400

    Add updated files

--- Bash_Calculator.sh
+++ Bash_Calculator.sh
@@ -1,14 +1,20 @@
+# welcome statement and prepare the variables for submission:
+
 echo "Welcome to this simple Calculator made using Bash. Please enter two numbers sequentially : "
 read i 
 read j 
-  
+
+#reveal the calculator options:
+
 echo "Enter Choice :"
 echo "1. Addition"
 echo "2. Subtraction"
 echo "3. Multiplication"
 echo "4. Division"
 read ch 
-  
+
+#perform the maths operations:
+
 case $ch in
   1)res=`echo $i + $j | bc` 
   ;; 
